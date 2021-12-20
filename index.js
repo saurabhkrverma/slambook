1a2cd69510ef76754edde8154ceb5f7def1cfe70
Saurabh Kumar Verma <sauraverma@paypal.com>
2021-12-20 22:50:16 +0530

    adding default process port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoose
     .connect("mongodb+srv://sauraverma:Dushyant%407@cluster0.ppbaa.mongodb.net/scrapbook?retryWrites=true&w=majority", {useNewUrlParser: true})
     .then(()=>{
         console.log("db connect up and running bro");
-        app.listen(5000, () =>{
+        app.listen((process.env.PORT || 5000), () =>{
             console.log("server is running on port 5000")
         })
     });
