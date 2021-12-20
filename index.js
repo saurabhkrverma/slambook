8616d970eb87dc471952c200cc6187f445ec8e32
Saurabh Kumar Verma <sauraverma@paypal.com>
2021-12-20 22:16:06 +0530

    connecting to db before starting the application

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ mongoose
     .connect("mongodb+srv://sauraverma:Dushyant%407@cluster0.ppbaa.mongodb.net/scrapbook?retryWrites=true&w=majority", {useNewUrlParser: true})
     .then(()=>{
         console.log("db connect up and running bro");
+        app.listen(5000, () =>{
+            console.log("server is running on port 5000")
+        })
     });
 
-app.listen(5000, () =>{
-    console.log("server is running on port 5000")
-})
+
 
 
 
