de7371f9da78056c16b9c794f45b70a4376ef8ff
shiv161 <shiv.poojan161@gmail.com>
2022-02-08 09:00:28 +0530

    fb login

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ export const DIST_DIR = path.join(__dirname, "./dist");
 
 // const mongoConnectionURI = `mongodb://localhost:27017/slambook`
 
-// const mongoConnectionURI = `mongodb+srv://sauraverma:Dushyant%407@cluster0.ppbaa.mongodb.net/slambook?retryWrites=true&w=majority`;
+const mongoConnectionURI = `mongodb+srv://sauraverma:Dushyant%407@cluster0.ppbaa.mongodb.net/slambook?retryWrites=true&w=majority`;
 
-const mongoConnectionURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ppbaa.mongodb.net/slambook?retryWrites=true&w=majority`;
+// const mongoConnectionURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ppbaa.mongodb.net/slambook?retryWrites=true&w=majority`;
 
 // middleware to use the json parser
 app.use(express.json());
@@ -62,10 +62,3 @@ try {
 } catch (error) {
     console.log(error);
 }
-
-
-
-
-
-
-
