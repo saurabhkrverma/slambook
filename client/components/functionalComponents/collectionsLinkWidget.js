eda318edad353eb36d20348a2c985825390b25ee
Saurabh Kumar Verma <sauraverma@paypal.com>
2022-03-10 22:18:45 +0530

    removing the relative path

--- client/components/functionalComponents/collectionsLinkWidget.js
+++ client/components/functionalComponents/collectionsLinkWidget.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Card, Nav } from "react-bootstrap";
+import {Row, Card, Nav, Image} from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const renderCollectionsLinkWidget = () => {
@@ -8,7 +8,7 @@ const renderCollectionsLinkWidget = () => {
             <Card className={"col-sm-10, col-md-3"}>
                 <Card.Body>
                     <div className={"add-slambook-icon add-slambook-icon-pulse"}>
-                        <button className="pulse-button"></button>
+                        <Image className="pulse-button" src={"/icons/add-slambook.png"} roundedCircle={true} fluid={true}></Image>
                     </div>
                     <div className={"add-slambook-options"}>
                         <Nav.Link as={Link} to="/collections">Create new slambook</Nav.Link>
